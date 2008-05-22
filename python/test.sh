293fdaca21f7ad0e93a17f486b7193e0339dd466
John Stowers <john.stowers@gmail.com>
2008-05-22 00:34:10 +0000

    init threads first in python version

--- python/test.sh
+++ python/test.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 PYTHONPATH=/home/john/Albatross/branches/groundstation/osm-gps-map/python/.libs \
     python -c \
-    'import gtk; import osmgpsmap; a = gtk.Window(); a.add(osmgpsmap.GpsMap()); a.show_all(); gtk.main()'
+    'import gtk; import osmgpsmap; gtk.threads_init(); a = gtk.Window(); a.add(osmgpsmap.GpsMap()); a.show_all(); gtk.main()'
