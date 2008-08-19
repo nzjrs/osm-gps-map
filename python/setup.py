20cfa4c05d10695bd1eec2409bc8d1fb16980bb8
John Stowers <john.stowers@gmail.com>
2008-08-19 00:53:13 +0000

    Can successfully build an installer on windows now

---
+++ python/setup.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+
+from distutils.core import setup, Extension
+
+def pkg_config_parse(pkg, opt):
+    import os
+    conf = os.popen('pkg-config %s %s' % (opt,pkg)).read()
+    opt = opt[-2:]
+    return [x.lstrip(opt) for x in conf.split()]
+
+def get_include(pkg):
+    return pkg_config_parse(pkg,'--cflags-only-I')
+
+def get_lib_dirs(pkg):
+    return pkg_config_parse(pkg,'--libs-only-L')
+
+def get_libs(pkg):
+    return pkg_config_parse(pkg,'--libs-only-l')
+
+#print get_libs('osmgpsmap pygobject-2.0')
+#print get_libs('osmgpsmap pygobject-2.0')
+#print get_libs('osmgpsmap pygobject-2.0')
+
+_osmgpsmap = Extension(name = 'osmgpsmap',
+            sources= ['osmgpsmapmodule.c','osmgpsmap.c'],
+            include_dirs = get_include('osmgpsmap pygobject-2.0'),
+            library_dirs = get_lib_dirs('osmgpsmap pygobject-2.0'),
+            libraries = get_libs('osmgpsmap pygobject-2.0'),
+        )
+
+setup( name = "pyosmgpsmap",
+    version = "0.1",
+    description = "python interface for osmgpsmap",
+    packages = ['osmgpsmap'],
+    ext_modules = [_osmgpsmap],
+    )
+
