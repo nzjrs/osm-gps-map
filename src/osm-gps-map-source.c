514139bf7cbc186e9fda3fc81fa2166ca4f6e52c
John Stowers <john.stowers@gmail.com>
2010-06-25 15:13:45 +1200

    Fix google

--- src/osm-gps-map-source.c
+++ src/osm-gps-map-source.c
@@ -79,7 +79,7 @@ osm_gps_map_source_get_repo_uri(OsmGpsMapSource_t source)
         case OSM_GPS_MAP_SOURCE_GOOGLE_HYBRID:
             return "http://mt#R.google.com/vt/lyrs=h@126&hl=en&x=#X&s=&y=#Y&z=#Z";
         case OSM_GPS_MAP_SOURCE_GOOGLE_SATELLITE:
-            return "http://khm#R.google.com/kh/v=61&x=#X&y=#Y&z=#Z";
+            return "http://khm#R.google.com/kh/v=63&x=#X&y=#Y&z=#Z";
         case OSM_GPS_MAP_SOURCE_VIRTUAL_EARTH_STREET:
             return "http://a#R.ortho.tiles.virtualearth.net/tiles/r#W.jpeg?g=50";
         case OSM_GPS_MAP_SOURCE_VIRTUAL_EARTH_SATELLITE:
