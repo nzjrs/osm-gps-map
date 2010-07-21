93f9dbd4000a6ad9b49ba52818c9878c24fd1a24
John Stowers <john.stowers@gmail.com>
2010-07-22 11:46:08 +1200

    Post release version bump

--- python/setup.py
+++ python/setup.py
@@ -17,7 +17,7 @@ def get_lib_dirs(pkg):
 def get_libs(pkg):
     return pkg_config_parse(pkg,'--libs-only-l')
 
-VERSION = "0.7.2"
+VERSION = "0.7.3"
 
 _osmgpsmap = Extension(name = 'osmgpsmap',
             sources= ['osmgpsmapmodule.c','osmgpsmap.c'],
