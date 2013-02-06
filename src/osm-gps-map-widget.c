30697da3678a760602affda13343b9314ff08762
John Stowers <john.stowers@gmail.com>
2013-02-06 13:58:45 +0100

    just disable the stupid extra border thing. ill fix this later

--- src/osm-gps-map-widget.c
+++ src/osm-gps-map-widget.c
@@ -141,7 +141,7 @@
 #include "osm-gps-map-compat.h"
 
 #define ENABLE_DEBUG                (0)
-#define EXTRA_BORDER                (TILESIZE / 2)
+#define EXTRA_BORDER                (0)
 #define OSM_GPS_MAP_SCROLL_STEP     (10)
 #define USER_AGENT                  "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.11) Gecko/20071127 Firefox/2.0.0.11"
 #define DOWNLOAD_RETRIES            3
